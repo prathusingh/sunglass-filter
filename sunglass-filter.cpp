0ced4dffc9219a55858a8f9e278500b116449772
Prathu Singh <prathusingh21@gmail.com>
2020-05-09 23:49:15 -0400

    Used naive replace

--- sunglass-filter.cpp
+++ sunglass-filter.cpp
@@ -1,45 +1,50 @@
+#include "sunglass-filter.h"
+
 #include <iostream>
-#include <utility>
-
 #include <opencv2/opencv.hpp>
-
-#include "sunglass-filter.h";
+#include <utility>
 
 using namespace cv;
 
-namespace sunglassfilter
-{
-SunglassFilter::SunglassFilter(std::string facePath, std::string sunglassPath)
-    : face(imread(facePath), sunglass(imread(sunglassPath))) {}
-
-void SunglassFilter::NaiveReplace(Mat &face_img, Mat &sunglass_img)
-{
-  std::cout << "naive replace" << std::endl;
+namespace sunglassfilter {
+SunglassFilter::SunglassFilter(std::string face_path, std::string sunglass_path) {
+    face = imread(face_path);
+    sunglass = imread(sunglass_path);
 }
 
-void SunglassFilter::UsingMask(Mat &face_img, Mat &sunglass_img, Mat &alpha_mask)
-{
-  std::cout << "using mask" << std::endl;
+std::pair<Bound, Bound> SunglassFilter::GetCoordinates(Mat &sunglass_img) {
+    Bound row_index_bound;
+    Bound col_index_bound;
+
+    row_index_bound.first = 160;
+    col_index_bound.first = 480;
+
+    row_index_bound.second = row_index_bound.first + sunglass_img.size().height;
+    col_index_bound.second = col_index_bound.first + sunglass_img.size().width;
+
+    return std::make_pair(row_index_bound, col_index_bound);
 }
 
-std::pair<std::pair<int, int>, std::pair<int, int>> SunglassFilter::GetCoordinates(Mat &sunglass_img)
-{
-  Bound row_index_bound;
-  Bound col_index_bound;
-
-  row_index_bound.first = 160;
-  col_index_bound.first = 480;
-
-  row_index_bound.second = row_index_bound.first + sunglass_img.size().height;
-  col_index_bound.second = col_index_bound.first + sunglass_img.size().width;
-
-  return std::make_pair(row_index_bound, col_index_bound);
+void SunglassFilter::NaiveReplace() {
+    Mat musk_with_naive_replace = face.clone();
+    auto coordinates_bounds = GetCoordinates(sunglass);
+    auto row_bounds = coordinates_bounds.first;
+    auto col_bounds = coordinates_bounds.second;
+    Mat roi_face = musk_with_naive_replace(Range(row_bounds.first, row_bounds.second),
+                                           Range(col_bounds.first, col_bounds.second));
+    // sunglass.copyTo(roi_face);
+    imshow("naive", roi_face);
+    waitKey(0);
 }
+}  // namespace sunglassfilter
 
-int main()
-{
-  std::string face_image_path = "../images/musk.png";
-  std::string sun_glass_path = "../images/sunglass.png";
-  SunglassFilter sunglass_filter{face_image_path, sun_glass_path};
+int main() {
+    std::string face_image_path = "../musk.png";
+    std::string sun_glass_path = "../images/sunglass.png";
+    Mat temp = imread(face_image_path);
+    imshow("tempwi", temp);
+    waitKey(0);
+    // sunglassfilter::SunglassFilter sunglass_filter{face_image_path, sun_glass_path};
+    // sunglass_filter.NaiveReplace();
+    return 0;
 }
-} // namespace sunglassfilter
--- sunglass-filter.h
+++ sunglass-filter.h
@@ -1,32 +1,29 @@
 #ifndef SUNGLASS_FILTER_H_
 #define SUNGLASS_FILTER_H_
 
+#include <opencv2/opencv.hpp>
 #include <string>
 #include <utility>
 
-#include <opencv2/opencv.hpp>
-
 using namespace cv;
 
-namespace sunglassfilter
-{
+namespace sunglassfilter {
 typedef std::pair<int, int> Bound;
 
-class SunglassFilter
-{
-private:
+class SunglassFilter {
+   private:
     Mat face;
     Mat sunglass;
 
-public:
+   public:
     SunglassFilter(std::string face_path, std::string sunglass_path);
 
-    void NaiveReplace(Mat &face_img, Mat &sunglass_img);
+    void NaiveReplace();
 
     void UsingMask(Mat &face_img, Mat &sunglass_img, Mat &alpha_mask);
 
     static std::pair<Bound, Bound> GetCoordinates(Mat &sunglass_img);
 };
-} // namespace sunglassfilter
+}  // namespace sunglassfilter
 
-#endif // SUNGLASS_FILTER_H_
+#endif  // SUNGLASS_FILTER_H_
